d46a39e4d599fd394f980374cb97492a1fa4e856
ss-koshala <koshala.manojeewa@silverstripers.com>
2020-06-16 06:14:49 +0530

    bug fix

--- _config.php
+++ _config.php
@@ -2,5 +2,5 @@
 use SilverStripe\Core\Injector\Injector;
 use \SilverStripe\Control\Email\Email;
 
-Injector::inst()->registerService(new SmtpMailer, 'Mailer');
+Injector::inst()->registerService(new \CustomSilvertripeMailer\SmtpMailer(), 'Mailer');
 Injector::inst()->create('Email', 'SMTPEmail');
